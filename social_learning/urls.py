7604ebdb15dc9cc39b1b376e5c93243e66f27124
Shivlal Sharma <40575397+sshiv5768@users.noreply.github.com>
2020-04-09 09:34:14 +0530

    updated urls

--- social_learning/urls.py
+++ social_learning/urls.py
@@ -22,8 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("home.urls")),
-    path('videos/',include('videos.urls')),
-
-    
     path('account/',include("account.urls")),
+    path('account/',include("django.contrib.auth.urls"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
